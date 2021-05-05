bbe3b6ac73aa5e8f508a2680c021f839c652c803
Wojttekja <79536275+Wojttekja@users.noreply.github.com>
2021-05-05 16:34:29 +0200

    Update backend.py

--- Android_App/backend.py
+++ Android_App/backend.py
@@ -17,4 +17,3 @@ with open("Android_App/mode.txt", "w") as file:
     file.write(on_off + "\n" + mode + "\n" + colour)
     
 s3.Object("light-control-app", "mode.txt").upload_file("Android_App/mode.txt")
-s3.Object("light-control-app", "whether.txt").upload_file("Android_App/yes.txt")
