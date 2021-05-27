91137ee93b6fd9bf0ee064770f62b2111d177347
Wojttekja <79536275+Wojttekja@users.noreply.github.com>
2021-05-27 11:21:41 +0200

    Update backend.py

--- Android_App/backend.py
+++ Android_App/backend.py
@@ -1,19 +1,14 @@
 import boto3
 import botostubs
-
-on_off = str(input("On?: "))
-if (on_off == "True"):
-    mode = str(input("Mode: "))
-    if (mode == "Static"):
-        colour = str(input("Colour: "))
+while True:
+    on_off = str(input("On?: "))
+    if (on_off == "True"):
+        on_off = True
     else:
-        colour = ""
-else:
-    mode = ""
-    colour = ""
+        on_off = False
 
-s3 = boto3.resource("s3")
-with open("Android_App/mode.txt", "w") as file:
-    file.write(on_off + "\n" + mode + "\n" + colour)
-    
-s3.Object("light-control-app", "mode.txt").upload_file("Android_App/mode.txt")
+    s3 = boto3.resource("s3")
+    with open("Android_App/mode.txt", "w") as file:
+        file.write(str(on_off))
+        
+    s3.Object("light-control-app", "mode.txt").upload_file("Android_App/mode.txt")
