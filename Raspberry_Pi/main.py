70052deb5d857728e7d71c62a5ec54f279ffe2b3
Wojttekja <79536275+Wojttekja@users.noreply.github.com>
2021-05-27 11:18:57 +0200

    Update main.py

--- Raspberry_Pi/main.py
+++ Raspberry_Pi/main.py
@@ -2,25 +2,43 @@ import boto3
 import botostubs
 from datetime import datetime
 from time import sleep
+import RPi.GPIO as GPIO
+
+def toggle ():
+    with open ("last_mode.txt", "r") as last:
+        last_str = last.read()
+    with open ("last_mode.txt", "w") as last:
+        if (last_str == "True"):
+            GPIO.output(21, GPIO.LOW)
+            last.write("False")
+        else:
+            GPIO.output(21, GPIO.HIGH)
+            last.write("True")
+        
 
 
 def check ():
     s3 = boto3.resource("s3")
-    with open("Raspberry_Pi/last_modified.txt", "r") as file:
+    with open("last_modified.txt", "r") as file:
         last = file.read()
     last = last.replace("-", "/")
     last = datetime.strptime(last, "%Y/%m/%d %H:%M:%S%z")
     now = s3.Object("light-control-app", "mode.txt").__getattribute__("last_modified")
     if (now > last):
-        with open("Raspberry_Pi/last_modified.txt", "w") as file:
+        with open("last_modified.txt", "w") as file:
             file.write(str(now))
         return True
     return False
-    
 
-if (check()):
-    s3 = boto3.resource("s3")
-    s3.meta.client.download_file("light-control-app", "mode.txt", "Raspberry_Pi/mode.txt")
-    print("pobralem")
-else:
-    print("Nie ma nic nowego")
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(21, GPIO.OUT)
+
+while True:
+    if (check()):
+        s3 = boto3.resource("s3")
+        print("Zmienilo sie", datetime.now())
+        toggle()
+    else:
+        print("Nie ma nic nowego", datetime.now())
+    sleep(5)
