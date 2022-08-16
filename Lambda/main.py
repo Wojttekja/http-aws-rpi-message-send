a05aecbba14efff167ad7324c4632b25c8a3ffa1
Wojttekja <79536275+Wojttekja@users.noreply.github.com>
2022-08-16 16:41:39 +0200

    Update main.py

--- Lambda/main.py
+++ Lambda/main.py
@@ -1,17 +1,15 @@
-from asyncio import events
 import json
 import boto3
 
 def lambda_handler(event, context):
-
+    nowe = str(event)[10:]
+    nowe = nowe[:len(nowe)-2]
     with open("/tmp/mode.txt", "w") as file:
-        file.write(str(event))
+        file.write(str(nowe))
     s3 = boto3.resource(u's3')
-    bucket = s3.Bucket(u'swiatla')
+    bucket = s3.Bucket(u'light-control-app')
     bucket.upload_file('/tmp/mode.txt', 'mode.txt')
-        
-    
     return {
         'statusCode': 200,
-        'body': json.dumps('Polsak')
+        'body': "udalo sie"
     }
